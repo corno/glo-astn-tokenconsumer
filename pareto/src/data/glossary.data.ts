64251b2b10a30f53d1128048b56d6411011b4c0c
corno <corno.astn.a@schraverus.com>
2023-03-28 10:17:53 +0200

    .

--- pareto/src/data/glossary.data.ts
+++ pareto/src/data/glossary.data.ts
@@ -24,46 +24,49 @@ export const $: g_glossary.T.Glossary<pd.SourceLocation> = {
         "Annotation": null,
     }),
     'imports': d({}),
-    'types': d({
-        "EndAnnotation": type(ref(glossaryParameter("Annotation"))),
-        "AnnotatedToken": type(group({
-            "token": member(ref(typeReference("Token"))),
-            "annotation": member(ref(glossaryParameter("Annotation"))),
-        })),
-        "MultilineStringData": type(group({
-            "lines": member(array(string())),
-        })),
-        "SimpleStringData": type(group({
-            "wrapping": member(ref(typeReference("Wrapping"))),
-            "value": member(string()),
-        })),
-        "StructuralTokenData": type(group({
-            "type": member(ref(typeReference("StructuralTokenType"))),
-        })),
-        "StructuralTokenType": type(taggedUnion({
-            "tagged union start": group({}),
-            "open shorthand group": group({}),
-            "close shorthand group": group({}),
-            "open verbose group": group({}),
-            "close verbose group": group({}),
-            "open dictionary": group({}),
-            "close dictionary": group({}),
-            "open list": group({}),
-            "close list": group({}),
-        })),
-        "Token": type(taggedUnion({
-            "header start": group({}),
-            "structural": ref(typeReference("StructuralTokenData")),
-            "simple string": ref(typeReference("SimpleStringData")),
-            "multiline string": ref(typeReference("MultilineStringData")),
-        })),
-        "Wrapping": type(taggedUnion({
-            "quote": group({}),
-            "apostrophe": group({}),
-            "none": group({}),
-        })),
+    'root': {
+        'namespaces': d({}),
+        'types': d({
+            "EndAnnotation": type(ref(glossaryParameter("Annotation"))),
+            "AnnotatedToken": type(group({
+                "token": member(ref(typeReference("Token"))),
+                "annotation": member(ref(glossaryParameter("Annotation"))),
+            })),
+            "MultilineStringData": type(group({
+                "lines": member(array(string())),
+            })),
+            "SimpleStringData": type(group({
+                "wrapping": member(ref(typeReference("Wrapping"))),
+                "value": member(string()),
+            })),
+            "StructuralTokenData": type(group({
+                "type": member(ref(typeReference("StructuralTokenType"))),
+            })),
+            "StructuralTokenType": type(taggedUnion({
+                "tagged union start": group({}),
+                "open shorthand group": group({}),
+                "close shorthand group": group({}),
+                "open verbose group": group({}),
+                "close verbose group": group({}),
+                "open dictionary": group({}),
+                "close dictionary": group({}),
+                "open list": group({}),
+                "close list": group({}),
+            })),
+            "Token": type(taggedUnion({
+                "header start": group({}),
+                "structural": ref(typeReference("StructuralTokenData")),
+                "simple string": ref(typeReference("SimpleStringData")),
+                "multiline string": ref(typeReference("MultilineStringData")),
+            })),
+            "Wrapping": type(taggedUnion({
+                "quote": group({}),
+                "apostrophe": group({}),
+                "none": group({}),
+            })),
 
-    }),
+        }),
+    },
     'asynchronous': {
         'interfaces': d({
             "TokenConsumer": aInterface(streamconsumer(
